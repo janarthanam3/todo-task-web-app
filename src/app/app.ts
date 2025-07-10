e2ef7756ec7c8d4b41165d617695456b7cded0f6
janarthanam3 <parameshjana333@gmail.com>
2025-07-11 05:28:23 +0530

    header component added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports:[Header],
   styleUrl: './app.css'
 })
 export class App {

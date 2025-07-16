1b3897bce564170e1ca1418e1fd1fa2f77163e59
janarthanam3 <parameshjana333@gmail.com>
2025-07-16 07:33:46 +0530

    task form html added

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { Header } from './header/header';
 import { MemberCard } from './member-card/member-card';
 import { TaskHeader } from "./task-header/task-header";
 import { TaskContent } from "./task-content/task-content";
+import { TaskCreateForm } from "./task-create-form/task-create-form";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [Header, MemberCard, TaskHeader, TaskContent],
+  imports: [Header, MemberCard, TaskHeader, TaskContent, TaskCreateForm],
   styleUrl: './app.css'
 })
 export class App {
